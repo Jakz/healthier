225274b8001450f00ce8fd2efa056629f63dbb1e
Jack <jack@pixbits.com>
2025-02-19 10:45:00 +0100

    some tests on unit conversions

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,10 @@
-﻿#include <nana/gui.hpp>
-#include <nana/gui/widgets/listbox.hpp>
+﻿//#include <nana/gui.hpp>
+//#include <nana/gui/widgets/listbox.hpp>
 
 #include <vector>
+#include <string>
 
-namespace quantity
+namespace qty
 {
   enum class Unit
   {
@@ -25,6 +26,8 @@ namespace quantity
   public:
     Exponent(Unit unit, int magnitude) : _unit(unit), _magnitude(magnitude) { }
     Exponent(Unit unit) : _unit(unit), _magnitude(1) { }
+
+    std::string caption() const;
   };
 
   struct Value
@@ -46,20 +49,76 @@ namespace quantity
     Quantity(Value value) : _value(value) { }
     Quantity(Value value, Exponent exponent) : _value(value), _exponents({ exponent }) { }
 
+    Quantity operator*(Unit unit) const { Quantity result = *this; result._exponents.push_back(Exponent(unit)); return result; }
+    Quantity operator*(Exponent exponent) const { Quantity result = *this; result._exponents.push_back(exponent); return result; }
+
+    std::string caption();
   };
 
   struct Quantities
   {
     static Quantity count(uint64_t amount) { return Quantity(Value(amount), Exponent(Unit::Count)); }
+
   };
 
+  struct Units
+  {
+    static Exponent milliliter() { return Exponent(Unit::Volume, -3); }
+  }
 
 }
 
+using namespace qty;
+
+std::string Exponent::caption() const
+{
+  switch (_unit)
+  {
+    case Unit::Count: return "";
+    case Unit::Volume:
+    {
+      switch (_magnitude)
+      {
+        case 1: return "L";
+        case 2: return "mL";
+        case 3: return "µL";
+        case 4: return "nL";
+        case 5: return "pL";
+        case 6: return "fL";
+        case 7: return "aL";
+        case 8: return "zL";
+        case 9: return "yL";
+        default: return "";
+      }
+    }
+  }
+
+  return "";
+}
+
+std::string Quantity::caption()
+{
+ std::string caption = std::to_string(_value.value);
+  for (auto& exponent : _exponents)
+  {
+    caption += " " + exponent.caption();
+  }
+  
+  return caption;
+}
+
+
+#include <iostream>
+
 int main()
 {
+  using namespace qty;
+  Quantity wbc = Quantities::count(6570);
+  Quantity wbcml = wbc * Units::milliliter();
+
+  std::cout << wbcml.caption() << std::endl;
   /* create a nana window and add a listbox to it */
-  nana::form fm;
+  /*nana::form fm;
   nana::listbox lb(fm, nana::rectangle(0, 0, 640, 480));
 
   lb.append_header("Description");
@@ -71,5 +130,5 @@ int main()
 
   fm.show();
 
-  nana::exec();
+  nana::exec();*/
 } 
